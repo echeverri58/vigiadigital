ef229381773bae7f3840efa1f127fc134a0505ab
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2023-11-24 19:18:00 -0500

    Update vigia.py

--- vigia.py
+++ vigia.py
@@ -9,82 +9,11 @@ from tqdm import tqdm
 
 # Función para descargar archivos PDF
 def download_pdfs():
-    # Comprobar si la carpeta "PDF" ya existe, y si no, crearla
-    if not os.path.exists('PDF'):
-        os.mkdir('PDF')
-    else:
-        st.warning("El directorio 'PDF' ya existe.")
-
-    # Por medio del Xpath extraemos la URL del mes actual de los editos de CORNARE
-    r = requests.get("https://www.cornare.gov.co/boletin-oficial/")
-    soup = BeautifulSoup(r.content, "html.parser")
-    dom = etree.HTML(str(soup))
-    url = dom.xpath('//*[@id="f935fbc7ba2b28a6f"]/div/ul/li[1]/a/@href')
-    url2 = str(url)
-    url3 = url2.strip("['/']")
-
-    response = requests.get(url3)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    links = soup.find_all('a')
-
-    i = 0
-    total_pdfs = sum('.pdf' in link.get('href', []) for link in links)
-
-    with st.empty():
-        for link in links:
-            if '.pdf' in link.get('href', []):
-                i += 1
-                pdf_url = link.get('href')
-                st.write(f"Descargando archivo {i}/{total_pdfs}: {pdf_url}")
-
-                # Intentar descargar el archivo
-                try:
-                    response = requests.get(pdf_url)
-                    pdf_path = os.path.join("PDF", f"pdf{i}.pdf")
-                    with open(pdf_path, 'wb') as pdf:
-                        pdf.write(response.content)
-                    st.write(f"Archivo {i} descargado y guardado en {pdf_path}")
-
-                    # Actualizar barra de progreso
-                    progress = i / total_pdfs
-                    st.progress(progress)
-
-                except Exception as e:
-                    st.error(f"Error al descargar el archivo {pdf_url}: {e}")
-
-    st.success("Archivos PDF descargados exitosamente.")
+    # Resto del código para descargar PDFs
 
 # Función para procesar archivos PDF descargados
 def process_downloaded_pdfs(keyword):
-    path = "PDF"
-    path2 = "separados"
-    files = os.listdir(path)
-    contador = 0
-    st.write(f"Buscando la palabra '{keyword}' en los archivos PDF descargados:")
-
-    for file in tqdm(files):
-        try:
-            doc = fitz.open(os.path.join(path, file))
-            encontrado = False
-
-            for page in doc:
-                text = page.get_text()
-                if keyword in text:
-                    page.add_highlight_annot(text.find(keyword), text.rfind(keyword), fill="red")
-                    encontrado = True
-
-            if encontrado:
-                doc.save(os.path.join(path2, file))
-                sleep(0.5)
-                contador += 1
-
-        except Exception as e:
-            st.error(f"Error al procesar el archivo {file}: {e}")
-            continue
-
-    st.success(f"De {len(files)} archivos analizados, {contador} tienen la palabra '{keyword}' resaltada.")
+    # Resto del código para procesar PDFs
 
 # Función para borrar todos los PDF descargados
 def delete_downloaded_pdfs():
@@ -95,10 +24,6 @@ def delete_downloaded_pdfs():
             file_path = os.path.join(path, file)
             os.remove(file_path)
         st.success("Archivos PDF descargados eliminados con éxito.")
-
-# Limpiar archivos PDF descargados al inicio de la aplicación
-if os.path.exists("PDF"):
-    delete_downloaded_pdfs()
 
 # Interfaz de usuario
 menu = st.sidebar.selectbox("Selecciona una opción:", ["Descargar PDFs", "Procesar PDFs", "Eliminar PDFs"])
@@ -121,5 +46,12 @@ elif menu == "Procesar PDFs":
 elif menu == "Eliminar PDFs":
     st.sidebar.header("Eliminar PDFs")
     st.sidebar.write("Haga clic en el botón para eliminar los archivos PDF descargados manualmente.")
+    
     if st.sidebar.button("Eliminar"):
-        delete_downloaded_pdfs()
+        # Se añade una confirmación del usuario antes de eliminar los PDFs
+        if st.checkbox("Confirmar eliminación"):
+            delete_downloaded_pdfs()
+            st.success("Archivos PDF descargados eliminados con éxito.")
+        else:
+            st.warning("Por favor, confirme la eliminación marcando la casilla.")
+
