4979ffdefd03338c5b348ff3f4b059c996d38c92
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2024-12-28 13:41:24 -0500

    Update vigia.py

--- vigia.py
+++ vigia.py
@@ -58,6 +58,7 @@ def buscar_palabra_clave_en_pdf(url, palabra_clave):
                     if paginas_con_palabra_clave:
                         resultados.append((link_href, paginas_con_palabra_clave))
                         encontrados += 1
+                        st.write(f"{encontrados}. La palabra clave '{palabra_clave}' se encontró en las páginas {paginas_con_palabra_clave} del PDF: {link_href}")
 
             except Exception as e:
                 st.write(f"No se pudo analizar el PDF en '{link_href}': {e}")
@@ -72,8 +73,6 @@ def buscar_palabra_clave_en_pdf(url, palabra_clave):
         st.write("No se encontró la palabra clave en ninguno de los PDFs revisados.")
     else:
         st.write("Todos los archivos PDF han sido revisados.")
-        for i, (link_href, paginas_con_palabra_clave) in enumerate(resultados, start=1):
-            st.write(f"{i}. La palabra clave '{palabra_clave}' se encontró en las páginas {paginas_con_palabra_clave} del PDF: {link_href}")
 
 st.set_page_config(page_title="Vigías del Río Dormilón", page_icon=":guardsman:", layout="wide")
 
